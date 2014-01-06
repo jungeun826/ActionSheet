e78c014bc47f5d9b177e4fa3e596f181d34527e4
jungeun826 <jungeun826@nate.com>
2014-01-06 13:37:49 +0900

    실습 3.2.2을 완료했다.
    2가지 actionSheet 생성을 실습했다.

--- ActionSheet/ViewController.m
+++ ActionSheet/ViewController.m
@@ -12,8 +12,31 @@
 
 @end
 
-@implementation ViewController
-
+@implementation ViewController{
+    UIActionSheet *sheet2;
+    int count;
+}
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    NSLog(@"selected index : %d", buttonIndex);
+}
+- (void)willPresentActionSheet:(UIActionSheet *)actionSheet{
+    if(actionSheet.tag == 2){
+        NSString *title = [NSString stringWithFormat:@"%d 번째", count++];
+        actionSheet.title = title;
+    }
+}
+-(IBAction)showBasicActionSheet:(id)sender{
+    UIActionSheet *sheet = [[UIActionSheet alloc]initWithTitle:@"제목" delegate:self cancelButtonTitle:@"취소" destructiveButtonTitle:@"중요" otherButtonTitles:@"확인1",@"확인2" , nil];
+    sheet.tag = 1;
+    [sheet showInView:self.view];
+}
+-(IBAction)showActionSheetWithCount:(id)sender{
+    if(sheet2 == nil){
+        sheet2 = [[UIActionSheet alloc] initWithTitle:@"제목" delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:@"선택1",@"선택2", nil];
+        sheet2.tag=2;
+    }
+    [sheet2 showInView:self.view];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
